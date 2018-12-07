15bcffb46d843335a163653a062e4b37663513c1
srewac <srewac@126.com>
2018-12-07 00:48:42 -0800

    Recommend engine integrate.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 from scipy import sparse
 import csv
-from Data.matrix_factorization_soln import *
 from enum import Enum
-
+from LFM import *
+import matplotlib.pyplot as plt
+from UserBasedModel import *
 
 class Genre(Enum):
     Action = 0
@@ -27,6 +28,13 @@ class Genre(Enum):
     Western = 17
 
 
+def genre_array():
+    genre_array = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime', \
+                   'Documentary', 'Drama', 'Fantasy', 'FilmNoir', 'Horror', 'Musical', \
+                   'Mystery', 'Romance', 'SciFi', 'Thriller', 'War', 'Western']
+    return genre_array
+
+
 def read_rating(fname, movie_len):
     rating_file = pd.read_csv(fname)
     user_mat_len = rating_file.user.max() - rating_file.user.min() + 1
@@ -40,10 +48,11 @@ def read_rating(fname, movie_len):
 
 
 def read_movie_genre(fname):
-    with open(fname, 'rb') as f:
+    '''with open(fname, 'rb') as f:
         movie_list = f.read().splitlines()
         movie_len = int(movie_list[movie_list.__len__() - 1].split("::")[0])
         movie_genre_mat = sparse.lil_matrix((movie_len, Genre.__len__()))
+        genre_distribute = [0 for i in range(18)]
         for movie in movie_list:
             movie_genre = movie.split("::")
             for genre in movie_genre[2].split('|'):
@@ -54,38 +63,69 @@ def read_movie_genre(fname):
                 if (genre == "Film-Noir"):
                     genre = "FilmNoir"
                 movie_genre_mat[int(movie_genre[0]) - 1, Genre[genre].value] = 1
+                genre_distribute[Genre[genre].value] += 1
     print("movie genre read complete")
-    return movie_len, movie_genre_mat
+    return movie_len, movie_genre_mat, genre_distribute
+    '''
+    movie = pd.read_csv(fname,sep='::')
+    movie_genre_mat = sparse.lil_matrix((movie.movieId.max()+1, Genre.__len__()))
+    genre_distribute = [0 for i in range(18)]
+    for _, row in movie.iterrows():
+        for genre in row.genres.split('|'):
+            if (genre == "Children's"):
+                genre = "Children"
+            if (genre == "Sci-Fi"):
+                genre = "SciFi"
+            if (genre == "Film-Noir"):
+                genre = "FilmNoir"
+            movie_genre_mat[row.movieId, Genre[genre].value] = 1
+            genre_distribute[Genre[genre].value] += 1
+    print("movie genre read complete")
+    return movie.movieId.max()+1, movie_genre_mat, genre_distribute
 
 def read_user_genre(rating_mat, movie_genre_mat):
 #Todo: compute user_genre base on their rating habit
     return
 
-
-def output_rating_CF(prediction_mat, ofname, first_user_idx):
-    with open(ofname, 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow(['user', 'movie', 'rating'])
-
-        row, col = prediction_mat.nonzero()
-        for i in range(len(row)):
-            writer.writerow([row[i] + first_user_idx, col[i] + 1, prediction_mat[row[i], col[i]]])
-        print("write file {0} complete").format(ofname)
+def plot_genre(genre):
+    plt.title("genre distribution")
+    plt.ylabel("occurance")
+    plt.xlabel("genre")
+    x = np.arange(18)
+    plt.bar(x, height=genre)
+    plt.xticks(x, genre_array())
+    plt.show()
+    print(genre)
     return
 
-
-def pred_one_user(recommender, user_id, offset):
-    out = recommender.user_mat[user_id] * recommender.movie_mat
-    # print("predict user {} using {} complete.").format(user_id+offset, recommender.name)
-    return out
-
-
-def pred_all_user(recommender):
-    out = recommender.user_mat * recommender.movie_mat
-    # print("predict all user using {} complete.").format(recommender.name)
-    return out
-
+def output_rating_CF(prediction_mat, ofname, offset):
+    with open(ofname, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(['user', 'movie', 'rating'])
+        row, col = prediction_mat.nonzero()
+        for i in range(len(row)):
+            writer.writerow([row[i] + offset, col[i] + 1, prediction_mat[row[i], col[i]]])
+            print("write file {0} complete").format(ofname)
+    return
 
 if __name__ == "__main__":
-    movie_len, movie_genre_mat = read_movie_genre("Data/movies.dat")
-    read_rating("Data/input.csv", movie_len)
+    movie_len, movie_genre_mat, movie_genre_distribute = read_movie_genre("movies.dat")
+    user_len, rating_mat, offset = read_rating("rating.csv", movie_len)
+
+    plot_genre(movie_genre_distribute)
+
+    rate_engine = MF_LFM()
+    rate_engine.learningLFM(rating_mat, 8, 2, 0.005, 0.02, 0.02)
+    '''
+    prediction = rate_engine.pred_all_user()
+    output_rating_CF(prediction, "test.csv", offset)
+    '''
+    print(rate_engine.top_n_recs(2789,10))
+
+    rate_engine2= UserBasedModel()
+    movies = pd.read_csv("movies.csv",sep='::')
+    ratings = pd.read_csv("ratings.csv")
+    rate_engine2.learning(ratings, movies, neighbourNumber=10, recommendationNumber=20)
+    rate_engine2.recommendByUser(2789)
+    rate_engine2.showTable()
+
